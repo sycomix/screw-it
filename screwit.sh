44f9d63db2cade32b0f4a8e9467fcd9b32d0e3bf
Del <6117352+sycomix@users.noreply.github.com>
2019-05-15 12:30:18 -0500

    Update screwit.sh

--- screwit.sh
+++ screwit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-(find * && exec cmake . && exec ./configure && make -Bkj 8 && make -Bkj 8 install && npm install && apt update && apt upgrade -y )
+( find * && exec cmake . && exec ./configure && make -Bkj 8 && make -Bkj 8 install && npm install && apt update && apt upgrade -y )
